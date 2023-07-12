5a5062d21b0e1c5fe737685e911b57cb81d15150
vincdac <91517220+vincdac@users.noreply.github.com>
2023-07-12 17:52:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Students from Batch 30");  
+     System.out.println("Hello Students from Batch 30 and raju");  
     }  
 } 
